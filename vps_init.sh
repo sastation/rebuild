78a265ee5a4ca77388f4af2b22058735da7476fd
David Wang <sa.station@gmail.com>
2026-07-23 16:37:16 +0800

    git config --global core.pager 'less -F -X'

--- vps_init.sh
+++ vps_init.sh
@@ -438,6 +438,7 @@ SetupUserEnv() {
         git config --global user.email "${GIT_EMAIL}"
         git config --global user.name "${GIT_NAME}"
         git config --global core.editor "vim"
+        git config --global core.pager 'less -F -X'
         git config --global push.default matching
     fi
 
